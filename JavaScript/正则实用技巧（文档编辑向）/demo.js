80e386a47c29d28d6755e70698eb8db957ad2f28
whistleStudio <whistleStudio@126.com>
2022-10-25 17:25:55 +0800

    202210251725

--- JavaScript/正则实用技巧（文档编辑向）/demo.js
+++ JavaScript/正则实用技巧（文档编辑向）/demo.js
@@ -1,5 +1,16 @@
 const fs = require("fs")
 
 let data = fs.readFileSync("test.html").toString()
-data = data.replace(RegExp(/我是/g), "")
-console.log(data)
+// data = data.replace(/我是/g, "")
+// fs.writeFile("test1.html", data, err => {
+//   if (!err) {
+//     console.log("删除成功")
+//   }
+// })
+
+data = data.replace(/(?<=我是)/g, "一个")
+fs.writeFile("test2.html", data, err => {
+  if (!err) {
+    console.log("添加成功")
+  }
+})
