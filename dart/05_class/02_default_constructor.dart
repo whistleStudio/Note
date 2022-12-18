a2496b37921ee069d31cc8a3a5b56337cc47cd7c
whistleStudio <435424527@qq.com>
2022-12-18 18:08:49 +0800

    202212181808

--- dart/05_class/02_default_constructor.dart
+++ dart/05_class/02_default_constructor.dart
@@ -1,11 +1,20 @@
 class Point {
-  num x=0, y=0;
+  num x = 0, y = 0;
   // 普通构造函数
-  Point (num x, num y) {
-    // 避免属性与形参歧义用this
-    this.x = x;
-    this.y = y;
-  }
+  // Point(num a, num b) {
+  //   x = a;
+  //   y = b;
+  // }
+  // Point(num x, num y) {
+  //   // 避免属性与形参歧义用this
+  //   this.x = x;
+  //   this.y = y;
+  // }
+  // 简写形式
+  Point(this.x, this.y);
 }
+
 void main(List<String> args) {
+  var p = Point(1, 2);
+  print(p.x);
 }
